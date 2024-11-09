ced07c66d4d653fc0ea82a2efa46ff7a4b1213b2
samuel maveke <mavekepeter2022@gmail.com>
2024-11-09 17:28:00 +0300

    add function to calculate and count time taken when song is playing and show totalTime and seekbar

--- src/context/playerContext.jsx
+++ src/context/playerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef,useState} from "react"
+import { createContext, useEffect, useRef,useState} from "react"
 import { songsData } from "../assets/assets";
 export const PlayerContext = createContext();
 
@@ -27,6 +27,25 @@ const PlayerContextProvider = (props) =>{
         audioRef.current.pause()
         setPlayStatus(false)
     }
+    useEffect(()=>{
+        setTimeout(() => {
+            audioRef.current.ontimeupdate = ()=>{
+                setTime({
+                    currentTime:{
+                        second:Math.floor(audioRef.current.currentTime % 60),
+                        minute:Math.floor(audioRef.current.currentTime / 60)
+                    },
+                    totalTime:{
+                        second:Math.floor(audioRef.current.duration % 60),
+                        minute:Math.floor(audioRef.current.duration / 60)
+                    }
+            
+                })
+            }
+
+        }, 1000);
+
+    },[audioRef])
     const contextvalue = {
         audioRef,
         seekBg,
